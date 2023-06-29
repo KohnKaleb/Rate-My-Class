74b5ce4334b64a20faff4ffe0d07cc2ce63e0591
kalebkohn <kjkohn@wisc.edu>
2023-06-28 21:08:25 -0500

    configured courses model to pull data from mysql database

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Courses(models.Model):
+    uuid = models.CharField(max_length=100, primary_key=True)
+    number = models.IntegerField()
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
